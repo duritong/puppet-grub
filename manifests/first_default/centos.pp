ee266f0f12145d3c5ac7ea0e329a5dfc7948c525
mh <mh@immerda.ch>
2012-01-06 13:29:12 +0100

    update augeas statement so it works with augeas-libs-0.10.0-2.el5.x86_64

--- manifests/first_default/centos.pp
+++ manifests/first_default/centos.pp
@@ -1,7 +1,8 @@
 class grub::first_default::centos {
   require grub
   augeas{'grub_first_default':
-    context => '/files/etc/grub.conf',
+    context => '/files/boot/grub/menu.lst',
     changes => 'set default 0',
+    onlyif => 'get default != 0',
   }
 }
