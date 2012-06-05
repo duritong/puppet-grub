92a8e72447482ce9572743969cec86230c87f6c9
mh <mh@immerda.ch>
2012-06-05 20:07:48 -0300

    new style for 2.7

--- manifests/first_default.pp
+++ manifests/first_default.pp
@@ -2,6 +2,6 @@ class grub::first_default{
   case $::operatingsystem {
     centos,redhat,fedora: { include grub::first_default::centos }
     debian: { /* this is not needed on debian based systems */ }
-    default: { info("No grub lense so far for $operatingsystem")}
+    default: { info("No grub lense so far for ${::operatingsystem}")}
   }
 }
