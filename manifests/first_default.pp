18f787847ab61a6f8de3c2572ea9c5b58d1a8a47
mh <mh@immerda.ch>
2015-11-28 17:41:27 +0100

    linting for future parser

--- manifests/first_default.pp
+++ manifests/first_default.pp
@@ -1,13 +1,7 @@
 # make the default first
 class grub::first_default{
-  case $::operatingsystem {
-    centos,redhat,fedora: {
-      # so far this is only needed pre el7
-      if $::operatingsystemmajrelease < 7 {
-        include grub::first_default::centos
-      }
-    }
-    debian: { /* this is not needed on debian based systems */ }
-    default: { info("No grub lense so far for ${::operatingsystem}")}
+  # so far this is only needed pre el7
+  if ($::operatingsystem == 'CentOS') and versioncmp($::operatingsystemmajrelease,'7') < 0 {
+    include ::grub::first_default::centos
   }
 }
